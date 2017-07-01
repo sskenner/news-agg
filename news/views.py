a954a3662b580a0843eefcd3bb5edd01778557ee
aria <nimda@arias-Air.home>
2017-06-30 21:28:39 -0400

    Add models, urls, views &bootstrap template

--- news/views.py
+++ news/views.py
@@ -2,5 +2,9 @@
 from __future__ import unicode_literals
 
 from django.shortcuts import render
-
+from .models import Article
 # Create your views here.
+
+def articles_list(request):
+    articles = Article.objects.all()
+    return render(request, 'news/articles_list.html', {'articles': articles})
