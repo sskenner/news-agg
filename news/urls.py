d64b07bb75d59c5c12e3c1d5719ed462555fd99d
Kenner <kenners@Corp.nychhc.org>
2017-07-03 13:17:43 -0400

    Update and create urls and list views

--- news/urls.py
+++ news/urls.py
@@ -2,5 +2,6 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^$', views.articles_list, name='articles_list')
+    url(r'^$', views.articles_list, name='articles_list'),
+    url(r'^feeds/', views.feeds_list, name='feeds_list')
 ]
--- news/views.py
+++ news/views.py
@@ -2,9 +2,13 @@
 from __future__ import unicode_literals
 
 from django.shortcuts import render
-from .models import Article
+from .models import Article, Feed
 # Create your views here.
 
 def articles_list(request):
     articles = Article.objects.all()
     return render(request, 'news/articles_list.html', {'articles': articles})
+
+def feeds_list(request):
+		feeds = Feed.objects.all()
+		return render(request, 'news/feeds_list.html', {'feeds': feeds})
