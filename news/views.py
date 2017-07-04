b1ff2c31407aac1f1b32721b4470228f2ce34582
aria <nimda@arias-Air.home>
2017-07-04 00:46:50 -0400

    Update admin articles and feeds format

--- news/views.py
+++ news/views.py
@@ -6,11 +6,17 @@ from .models import Article, Feed
 from .forms import FeedForm
 from django.shortcuts import redirect
 
+import feedparser
+import datetime
+
 # Create your views here.
 
 def articles_list(request):
     articles = Article.objects.all()
-    return render(request, 'news/articles_list.html', {'articles': articles})
+
+    rows = [articles[x:x+1] for x in range(0, len(articles), 1)]
+
+    return render(request, 'news/articles_list.html', {'rows': rows})
 
 def feeds_list(request):
 	feeds = Feed.objects.all()
@@ -21,12 +27,31 @@ def new_feed(request):
 		form = FeedForm(request.POST)
 		if form.is_valid():
 			feed = form.save(commit=False)
+
+			feedData = feedparser.parse(feed.url)
+
+
 			# set some fields
-			feed.title = "title"
+			feed.title = feedData.feed.title
 			feed.save()
+
+			for entry in feedData.entries:
+				article = Article()
+				article.title = entry.title
+				article.url = entry.link
+				article.description = entry.description
+				
+				d = datetime.datetime(*(entry.published_parsed[0:6]))
+				dateString = d.strftime('%Y-%m-%d %H:%M%:%S')
+
+				article.publication_date = dateString
+				article.feed = feed # why the parent-child relationship?
+				article.save()
+
 			return redirect('news.views.feeds_list') #not redirecting to feeds_list after save
 	else:
 		form = FeedForm()
 	return render(request, 'news/new_feed.html', {'form': form})
 
 # https://www.djangoproject.com/rss/weblog
+#42:54
